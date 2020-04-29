8c8be44c96d115c2af18028149a7060bf84f388c
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-04-29 23:43:33 +0100

    Yoga Setup

---
+++ backend/src/db.js
@@ -0,0 +1,13 @@
+//This file connects to the remote DB and gives us the ability to query it with JS
+//typeDefs is the generated GraphQl Api from prisma 
+//for the binding to work, it needs access to the generated Api for CRUD implementations
+const { Prisma } = require('prisma-binding')
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+})
+
+module.exports = db;
