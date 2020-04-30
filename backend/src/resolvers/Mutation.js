548151dc45241e21a3bdd4ed9504d5df56384bcc
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-04-30 22:27:53 +0100

    first query and mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const mutations = {};
+const mutations = {
+  createTesla: (parent, args) => {
+    global.cars = global.cars || []
+
+    const newCar = { name: args.name, model: args.model }
+    
+    global.cars.push(newCar)
+
+    return newCar
+    // console.log(args);
+
+  }
+};
 
 module.exports = mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,8 @@
-const Query = {};
+const Query = {
+  cars: () => {
+    global.cars = global.cars || []
+    return global.cars
+  }
+};
 
 module.exports = Query;
