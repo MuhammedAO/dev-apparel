caca1d1e15cebf77168f6972e2ece648a667936d
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-05-09 02:04:54 +0100

    update item mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,12 +1,30 @@
 const Mutations = {
-  async createItem (parent, args, context, info)  {
+  async createItem(parent, args, context, info) {
     const item = await context.db.mutation.createItem({
       data: {
         ...args
       }
     }, info)
     return item
-  }
+  } ,
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
 };
 
+//the info param allows the query e.g updateItem to know exactly what to return e.g Item!..
+//it will contain the query from the client side to return 'that' 'Item'.
 module.exports = Mutations;
