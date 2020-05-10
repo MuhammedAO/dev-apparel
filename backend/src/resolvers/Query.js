b77f1e80c8678f7b6bbbc9106897474a1dfc92d8
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-05-10 03:45:30 +0100

    Schema and query for pagination

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,6 +3,7 @@ const {forwardTo} = require('prisma-binding')
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
 // items: async (parent,args, context, info) => {
 //   const items = await context.db.query.items()
 //   return items
