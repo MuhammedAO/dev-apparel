9e63dffcb75473e9a21fda93fc620d32bfb360ed
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-05-13 01:41:15 +0100

    express cookie middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
+const cookieParser = require('cookie-parser')
 require('dotenv').config({ path: 'variables.env' })
 const createServer = require('./creatServer')
 const db = require('./db')
 
 const server = createServer();
+
+//use express middleware to handle cookies(JWT)
+//every single time that someone requests a page, the cookie is going to send along..
+//something called a jsonWebToken for current user validation
+server.express.use(cookieParser())
 
 server.start({
   cors: {
@@ -10,6 +16,6 @@ server.start({
     origin: process.env.FRONTEND_URL
   }
 }, deets => {
-  console.log(`Server is now running on http://locahost:${deets.port}`)
+  console.log(`Server is now running on http://localhost:${deets.port}`)
 
 })
