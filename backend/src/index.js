bada6b22bf28ff7343f9a57d69fa7523727f72f9
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-05-13 23:50:27 +0100

    Currently logged in user middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
 require('dotenv').config({ path: 'variables.env' })
 const createServer = require('./creatServer')
@@ -9,6 +10,22 @@ const server = createServer();
 //every single time that someone requests a page, the cookie is going to send along..
 //something called a jsonWebToken for current user validation
 server.express.use(cookieParser())
+
+//decode the jwt to get the userId on each request
+//this means that if we ever need to figure out who the current user is,
+//we can query the Db having already known the userId
+server.express.use((req, res, next) => {
+  const { token } = req.cookies
+
+  if (token) {
+    //userId is the payload gotten from the token
+    const { userId } = jwt.verify(token, process.env.APP_SECRET)
+
+    //add the userId to the request for further request accessibility
+    req.userId = userId
+  }
+  next()
+})
 
 server.start({
   cors: {
