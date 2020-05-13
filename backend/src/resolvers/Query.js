1829ef40a6698407e216c2cf3a6c1436fe68a3fc
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-05-13 23:50:59 +0100

    resolver for currently logged in user

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,13 +1,20 @@
-const {forwardTo} = require('prisma-binding')
+const { forwardTo } = require('prisma-binding')
 
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
-// items: async (parent,args, context, info) => {
-//   const items = await context.db.query.items()
-//   return items
-// }
+  me(parent, args, ctx, info) {
+    //check if there is a current userId
+    //the userId(decoded) is being passed unto the ctx via a request from middleware
+    if (!ctx.request.userId) {
+      return null
+    }
+    return ctx.db.query.user({
+      where: { id: ctx.request.userId }
+    }, info)
+  }
+  
 };
 
 module.exports = Query;
