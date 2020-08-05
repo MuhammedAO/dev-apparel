79d1949895e96c7c8379dcdfd83743645ab4b4ac
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-08-05 17:26:40 +0100

    Snapshot Testing

--- frontend/__tests__/item.test.js
+++ frontend/__tests__/item.test.js
@@ -1,5 +1,6 @@
 import ItemComponent from '../components/Item'
 import { shallow } from 'enzyme'
+import toJSON from 'enzyme-to-json'
 
 const fakeItem = {
   id: 'ABC123',
@@ -19,12 +20,20 @@ const fakeItem = {
 // debug() => Returns an html-like string of the wrapper for debugging purposes. Useful to print out to the console when tests are not passing when you expect them to.
 
 //dive() => Shallow renders one level deeper, .children() can also be used.
+
+
+//To write a snapshot test, you first get your code working, say, a React component, then generate a snapshot of it’s expected output given certain data. The snapshot tests are committed alongside the component and every time the tests are run. Jest will compare the snapshot to the rendered output for the test.
+
+//Snapshot tests are a very useful tool whenever you want to make sure your UI does not change unexpectedly. A typical snapshot test case for a mobile app renders a UI component, takes a snapshot, then compares it to a reference snapshot file stored alongside the test.
+
+//If the test does not pass, it may mean that there were some unexpected changes on the component that you need to fix, or you made some changes to the component and it’s about time you updated the snapshot tests.
+
+//toJson helper is used to convert any Enzyme wrapper to a format compatible with Jest snapshot.
+
 describe('<Item/>', () => {
-  it('it renders and displays properply', () => {
+  it('it renders and matches the snapshot', () => {
     const wrapper = shallow(<ItemComponent item={fakeItem}/>)
-    // const PriceTag = wrapper.find('PriceTag')
-    // console.log(PriceTag.dive().text())
-    // console.log(wrapper.debug())
+    expect(toJSON(wrapper)).toMatchSnapshot()
   })
 
   it('renders the image properly', () => {
