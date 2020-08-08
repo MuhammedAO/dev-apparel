2dc2de39b7a50fdbdbb26bff488b16784bc22bff
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-08-08 13:20:55 +0100

    endpoint

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,6 +1,6 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
-import { endpoint } from '../config';
+import { endpoint, prodEndpoint } from '../config';
 import {LOCAL_STATE_QUERY} from '../components/Cart'
 
 
@@ -8,13 +8,13 @@ import {LOCAL_STATE_QUERY} from '../components/Cart'
 
 const createClient = ({ headers }) => {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+    uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
     request: operation => {
       operation.setContext({
         fetchOptions: {
           credentials: 'include',
         },
-        headers,
+        headers, 
       });
     },
     //local data
